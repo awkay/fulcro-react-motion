1f0c42aefc370640ba75e789861be0742a140c5c
Tony Kay <tony.kay@gmail.com>
2018-01-31 17:15:08 -0800

    tuned up builds

--- project.clj
+++ project.clj
@@ -7,12 +7,10 @@
                  [org.clojure/clojurescript "1.9.946"]
                  [fulcrologic/fulcro "2.1.5-SNAPSHOT"]
                  [fulcrologic/fulcro-css "2.0.0"]
-                 [fulcrologic/fulcro-spec "2.0.0-beta3" :scope "test" :exclusions [fulcrologic/fulcro]]]
+                 [binaryage/devtools "0.9.8"]
+                 [thheller/shadow-cljs "2.0.150"]
+                 [fulcrologic/fulcro-inspect "2.0.0-alpha5"]
+                 [org.clojure/core.async "0.3.465"]
+                 [devcards "0.2.4" :exclusions [cljsjs/react cljsjs/react-dom]]]
 
-  :source-paths ["src/cards"]
-
-  :profiles {:cljs {:source-paths ["src/cards"]
-                    :dependencies [[binaryage/devtools "0.9.8"]
-                                   [thheller/shadow-cljs "2.0.150"]
-                                   [fulcrologic/fulcro-inspect "2.0.0-alpha5"]
-                                   [devcards "0.2.4" :exclusions [cljsjs/react cljsjs/react-dom]]]}})
+  :source-paths ["src/cards"])
